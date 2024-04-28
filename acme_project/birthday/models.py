40f04f2fde6e324a4b0e5f07202489bc4b2e630b
stepaxvii <153988379+stepaxvii@users.noreply.github.com>
2024-04-28 10:42:11 +0300

    create form by model birthday

--- acme_project/birthday/models.py
+++ acme_project/birthday/models.py
@@ -1,1 +1,17 @@
 from django.db import models
+
+
+class Birthday(models.Model):
+    first_name = models.CharField(
+        verbose_name='Имя',
+        max_length=20
+    )
+    last_name = models.CharField(
+        verbose_name='Фамилия',
+        blank=True,
+        help_text='Необязательное поле',
+        max_length=20
+    )
+    birthday = models.DateField(
+        verbose_name='Дата рождения'
+    )
